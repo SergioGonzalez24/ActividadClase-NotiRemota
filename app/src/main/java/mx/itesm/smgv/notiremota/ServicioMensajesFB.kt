4d3b7d3e0356324d553e67be7a70e05afbd7b263
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-09-28 15:21:06 -0500

    .

--- app/src/main/java/mx/itesm/smgv/notiremota/ServicioMensajesFB.kt
+++ app/src/main/java/mx/itesm/smgv/notiremota/ServicioMensajesFB.kt
@@ -13,3 +13,5 @@ class ServicioMensajesFB : FirebaseMessagingService() {
         println("Llega notificaccion remota!!!!!!!!!!!")
     }
 }
+
+// TOKEN: dNrsf8UjT_OvZwzopbq0fQ:APA91bEUzifhxTtZFlbXlEoF4HI8DX-DEr7CjKx9XZEeJso1yUFPSipAb_ffHpOPHeN3Nfqqxjidsezh3vz3_Mk9sRxQ83_1DqRO9zeWOvUBys-AWqhKFwZhmthTr6zzrJp7lTHg78uA
